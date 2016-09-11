6cf46863e933e76c0128169997bcce6825dcfc23
khai96_ <hvksmr1996@gmail.com>
2016-09-11 13:48:21 +0700

    add test

--- test/index.js
+++ test/index.js
@@ -3,6 +3,16 @@
 const PromiseSet = require('..')
 const {XPromiseSet} = PromiseSet
 
+class CompatibilityError extends Error {
+  get name () {
+    return 'CompatibilityError'
+  }
+}
+
+if (XPromiseSet !== require('../x')) {
+  throw new CompatibilityError("`require('promise-set').XPromiseSet` must return 'require('x-promise-set/x')'")
+}
+
 function testnormal (CPrmSet = PromiseSet) {
   const prmset = new CPrmSet()
   console.log(prmset)
