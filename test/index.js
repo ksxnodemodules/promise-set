44c2a1e5d5e4550d2749d4aa1559ef360fdcf01e
khai96_ <hvksmr1996@gmail.com>
2016-09-12 16:17:55 +0700

    update tester

--- test/index.js
+++ test/index.js
@@ -2,18 +2,28 @@
 
 const {stdout, stderr, exit} = require('process')
 const assert = require('assert')
+const {XIterable} = require('x-iterable-base')
 const PromiseSet = require('..')
 const {console} = global
 const {XPromiseSet} = PromiseSet
 const a2x = x => [x, x]
+const CSet = XIterable(Set)
+
+class ExpectationError extends Error {
+  get name () {
+    return 'ExpectationError'
+  }
+}
 
 function testnormal (CPrmSet = PromiseSet) {
   const {XPromise} = CPrmSet
-  const step = (object, count = 0) => {
+  const step = (object, count = 0, ...expectrest) => {
     console.log(`Begining step ${count}`, object)
     return {
-      nextStep: (act, expect) => {
-        const pass = () => {
+      nextStep: (act, expect, ...expectrest) => {
+        let nextexpectrest
+        const pass = rest => {
+          nextexpectrest = rest || ''
           stdout.write(`Passed step ${count}\n`)
         }
         const fail = error => {
@@ -25,11 +35,11 @@ function testnormal (CPrmSet = PromiseSet) {
           const nextObject = act(object, step)
           new Promise(
             (resolve, reject) =>
-              expect({object: nextObject, resolve, reject})
+              expect({object: nextObject, resolve, reject}, ...expectrest)
           )
-            .then(success)
+            .then(pass)
             .catch(fail)
-          return step(nextObject, count + 1)
+          return step(nextObject, count + 1, ...nextexpectrest)
         } catch (error) {
           fail(error)
         }
@@ -45,10 +55,35 @@ function testnormal (CPrmSet = PromiseSet) {
     .nextStep(
       prmset =>
         prmset.map(...a2x(x => x.charCodeAt())),
-      ({object, resolve, reject}) => {
-        resolve()
-      }
+      param =>
+        assertResultSet(param, new CSet('abcdefABCDEF'), new CSet('ghijklGHIJKL'))
     )
+    .nextStep(
+      prmset =>
+        prmset.filter(...a2x(x => x & 1)),
+      (param, prevResolveExpectation, prevRejectExpectation) =>
+        assertResultSet(param, prevResolveExpectation.filter(x => x & 1), prevRejectExpectation.filter(x => x & 1))
+    )
+    function assertResultSet ({object, resolve, reject}, resolveExpectation, rejectExpectation) {
+      const resolveActual = new CSet()
+      const rejectActual = new CSet()
+      const failReasonNeg = (value, state) =>
+        reject(new ExpectationError(`Detected '${value}' which shouldn't be ${state}`))
+      const failproc = (unexpectation, state, dest) =>
+        x => unexpectation.has(x) ? failReasonNeg(x, state) : dest.add(x)
+      const failifresolved = failproc(rejectExpectation, 'rejected', resolveActual)
+      const failifrejected = failproc(resolveExpectation, 'resolved', rejectActual)
+      const testneg = object.map(failifrejected, failifresolved)
+      Promise.all(testneg).then(() => {
+        try {
+          assert.deepStrictEqual([...resolveActual], [...resolveExpectation])
+          assert.deepStrictEqual([...rejectActual], [...rejectExpectation])
+          resolve([resolveExpectation, rejectExpectation])
+        } catch (error) {
+          reject(error)
+        }
+      })
+    }
 }
 
 function testadvance () {
