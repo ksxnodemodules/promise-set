69e50388d2b3bb720055e566d9b05bdeee6133d8
khai96_ <hvksmr1996@gmail.com>
2016-09-17 16:19:58 +0700

    make random string more awesome

--- sh/lib/test-count-log.js
+++ sh/lib/test-count-log.js
@@ -1,2 +1,8 @@
 'use strict'
-require('process').stdout.write([...require('crypto').prng(32)].map(String.fromCharCode).join(''))
+const {fromCharCode} = String
+const aCharCode = 'a'.charCodeAt()
+const i2c = x => fromCharCode(x + aCharCode)
+require('process').stdout.write(
+  [...require('crypto').prng(32)]
+    .map(byte => i2c(byte >> 4) + i2c(byte & 0b1111)).join('')
+)
