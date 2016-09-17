71e9b6669a36bb1e5b404bcb9e35c8c942f842ef
khai96_ <hvksmr1996@gmail.com>
2016-09-17 15:51:43 +0700

    verification messages end with newline

--- test/verify.js
+++ test/verify.js
@@ -6,10 +6,10 @@ const expected = 2 * 3
 
 const count = readFileSync(TEST_COUNT_LOGS, {encoding: 'utf8'}).split(TEST_COUNT_LOGS_DELEMITER).filter(Boolean).length
 if (count === expected) {
-  stdout.write('VERIFICATION PASSED!')
+  stdout.write('VERIFICATION PASSED!\n')
   exit(0)
 } else {
-  stderr.write('VERIFICATION FAILED!')
-  stdout.write(`Expecting ${expected} steps being tested but got ${count}`)
+  stderr.write('VERIFICATION FAILED!\n')
+  stdout.write(`Expecting ${expected} steps being tested but got ${count}\n`)
   exit(2)
 }
